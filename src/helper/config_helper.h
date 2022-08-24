51f3e06e038a51b65a7ca5d24459c100282e0024
= <=>
2022-08-24 14:18:17 +0200

    init tool is now completly contained in init.c/init.h no dependency on config_helper.h anymore

--- src/helper/config_helper.h
+++ src/helper/config_helper.h
@@ -5,23 +5,9 @@
 #include <stdio.h>
 #include <string.h>
 
-#define TYPE_DIR  0
-#define TYPE_FILE 1
-#define TYPE_CMD  2
-
-typedef struct init_config_t init_config_t;
-struct init_config_t
-{
-    char *name;
-    char *content;
-    char *(*content_func)();
-    uint8_t type;
-};
-
 void run_tool(int argc, char** argv);
 
-typedef void(*tool_function_t)(int argc, char** argv);
-
+typedef void (*tool_function_t)(int argc, char** argv);
 typedef struct tool_t tool_t;
 struct tool_t {
     char* name;
@@ -31,9 +17,5 @@ struct tool_t {
 
 extern tool_t config_tools[];
 extern uint32_t config_tools_len;
-extern char config_default_beaver[];
-extern char config_default_main[];
-extern init_config_t init_config[];
-extern uint32_t init_config_len;
 
 #endif
--- src/tools/init.h
+++ src/tools/init.h
@@ -4,9 +4,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//#include "../helper/config_helper.h"
 #include "../helper/helper.h"
 #include "../lib/argparse.h"
-#include "../helper/config_helper.h"
+
+#define TYPE_DIR ((uint8_t)0)
+#define TYPE_FILE ((uint8_t)1)
+#define TYPE_CMD ((uint8_t)2)
+
+typedef struct init_config_t init_config_t;
+struct init_config_t {
+    char* name;
+    char* content;
+    char* (*content_func)();
+    uint8_t type;
+};
+
+extern init_config_t init_config[];
+extern uint32_t init_config_len;
 
 void init(int argc, char** argv);
 
